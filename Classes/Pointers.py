7acfc54fc37568e76fcba86b38fb51fc92ddcfa5
NABrown843 <87168340+NABrown843@users.noreply.github.com>
2022-02-27 12:19:06 -0500

    Classes And Pointers

---
+++ Classes/Pointers.py
@@ -0,0 +1,27 @@
+num1 = 11
+
+num2 = num1
+
+print('Before value is updated: ')
+print('num1 = ', num1)
+print('num2 = ', num2)
+
+num1 = 22
+
+print('\nAfter value is updated: ')
+print('num1 = ', num1)
+print('num2 = ', num2)
+
+dict1 = {'value' : 11}
+
+dict2 = dict1
+
+print('Before value is updated: ')
+print('dict1 = ', dict1)
+print('dict2 = ', dict2)
+
+dict1['value'] = 22
+
+print('\nAfter value is updated: ')
+print('dict1 = ', dict1)
+print('dict2 = ', dict2)
